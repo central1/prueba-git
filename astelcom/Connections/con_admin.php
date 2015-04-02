da52cd2f1d711de7f1ae000e7dcb54710a54372c
chilambalam <bguzman@centralinteractiva.com.mx>
2015-04-02 17:13:23 -0600

    Update con_admin.php

--- astelcom/Connections/con_admin.php
+++ astelcom/Connections/con_admin.php
@@ -4,7 +4,7 @@
 # HTTP="true"
 $hostname_con_admin = "localhost";
 $database_con_admin = "paginade_astelcom";
-$username_con_admin = "paginade_astel";
+$username_con_admin = "paginade_prueba";
 $password_con_admin = "%W6[9SuX5F~X";
 $con_admin = mysql_pconnect($hostname_con_admin, $username_con_admin, $password_con_admin) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
